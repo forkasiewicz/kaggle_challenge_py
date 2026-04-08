95b94bb01e9806d96772f52abdc2b12adbed657c
forkasiewicz <111523660+forkasiewicz@users.noreply.github.com>
2026-04-08 14:40:27 +0200

    rmse calculation

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ import pandas as pd
 import numpy as np
 
 class DataProcessor:
-    def __init__(self, file_path: str) -> None:
+    def __init__(self: "DataProcessor", file_path: str) -> None:
         self.file_path: str = file_path
         self.df: pd.DataFrame = pd.read_csv(file_path, index_col=0)
 
-    def one_hot(self, *columns: str) -> None:
+    def one_hot(self: "DataProcessor", *columns: str) -> None:
         for column in columns:
             one_hot_columns: pd.DataFrame = pd.get_dummies(self.df[column], prefix=column)
             self.df = self.df.drop(columns=[column])
             self.df = self.df.join(one_hot_columns)
 
-    def scale(self, *columns: str) -> None:
+    def scale(self: "DataProcessor", *columns: str) -> None:
         for column in columns:
             col_min = self.df[column].min()
             col_max = self.df[column].max()
             self.df[column] = (self.df[column] - col_min) / (col_max - col_min)
 
-    def split_data(self, train_size: float) -> tuple[pd.DataFrame, pd.DataFrame]:
+    def split_data(self: "DataProcessor", train_size: float) -> tuple[pd.DataFrame, pd.DataFrame]:
         row_count: int = len(self.df)
         train_row_count: int = int(train_size * row_count)
 
@@ -33,14 +33,19 @@ class DataProcessor:
         return train_df, hidden_df
 
 class LinearRegression:
-    def __init__(self, df: pd.DataFrame, target: str) -> None:
+    def __init__(self: "LinearRegression", df: pd.DataFrame, target: str) -> None:
         self.X: np.ndarray = df.drop(columns=target).to_numpy()
-
         self.y: np.ndarray = df[target].to_numpy()
-
+        self.w: np.ndarray = np.zeros((self.X.shape[1], 1))
         self.b: float = 0.0
 
-        self.w = np.zeros((df.shape[1], 1))
+    def predict(self: "LinearRegression", X: np.ndarray) -> np.ndarray:
+        return (X @ self.w) + self.b
+        
+    def calculate_rmse(self: "LinearRegression", X: np.ndarray, y: np.ndarray) -> float:
+        y_hat: np.ndarray = self.predict(X)
+        mse = np.mean((y_hat - y) ** 2)
+        return np.sqrt(mse)
 
 if __name__ == "__main__":
     csv: DataProcessor = DataProcessor("diamonds.csv")
@@ -49,15 +54,14 @@ if __name__ == "__main__":
     
     train_df, hidden_df = csv.split_data(train_size=0.8)
 
-    print(csv.df.shape)
-
     # linear_regression = LinearRegression(train_df, target="price")
 
     # to_csv("answers_Forkasiewicz.csv")
 
 """
+initialize weights and bias (all zeros)
+
 loop:
-    initialize weights and bias (all zeros)
     calculate y_hat (matrix multiplication) y_hat = X@w + b
     calculate rmse
     calculate derivatives of w and b 
